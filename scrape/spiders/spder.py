4512aebd2d80edbb860b85b12defcc01d2a35f1c
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2023-03-24 15:24:45 -0700

    added moxing textbox and url

---
+++ scrape/spiders/spder.py
@@ -0,0 +1,44 @@
+import random
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "amz"
+
+    def __init__(self, domain = None, *args, **kwargs):
+        super(QuotesSpider, self).__init__(*args, **kwargs)
+        self.urlx = domain.split(',')
+        print("THE URL", self.urlx)
+
+    
+    def start_requests(self):
+
+        UA_STRINGS = [
+    "Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36\
+        (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36\
+        (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15\
+        (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1"
+        ]
+
+        HEADERS = {
+        'User-Agent': random.choice(UA_STRINGS),
+        'Accept-Language': 'en-US, en;q=0.5'
+        }
+
+        # urls = ['https://www.amazon.com/Jackson-Safety-Nemesis-22475-Anti-Fog/product-reviews/B008D81A0A/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews',
+        #         'https://www.amazon.com/Daisy-Jones-Taylor-Jenkins-Reid/product-reviews/1524798649/?_encoding=UTF8&pd_rd_w=NCCGT&content-id=amzn1.sym.64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_p=64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_r=P0CSWMGQD4WBNFV20N16&pd_rd_wg=gfa1J&pd_rd_r=db04e1dc-4f1e-485a-9c91-cd7d3090de03&ref_=pd_gw_crs_zg_bs_283155'
+        # ]
+
+        for url in self.urlx:
+            yield scrapy.Request(url=url, headers=HEADERS, callback=self.parse)
+
+    def parse(self, response):
+        total_ratings = response.css('div[data-hook]::text').get().strip()
+        review = response.css('span[data-hook]::text').get()
+
+
+        yield {
+            'total_rating' : review[:review.find(' ')],
+            'review' : total_ratings[:total_ratings.find(' ')],
+        }
--- search/views.py
+++ search/views.py
@@ -1,7 +1,76 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import asyncio
+from requests_html import AsyncHTMLSession
+import subprocess
+from scrape.spiders.spder import QuotesSpider
 
 # Create your views here.
 
 
+# def wo(url):
+#     s = HTMLSession()
+#     r = s.get(url)
+#     r.html.render(sleep=1)
+
+
+#     #This is our simple scraper, just copy xPath from inspect then paste it and follow this format
+#     data = {
+#         'total-ratings' : r.html.xpath('//*[@id="cm_cr-product_info"]/div/div[1]/div[3]/span', first=True).text,
+#         'outof5' : r.html.xpath('//*[@id="cm_cr-product_info"]/div/div[1]/div[2]/div/div/div[2]/div/span', first=True).text
+#     }
+
+#     print(data)
+
+# loop = asyncio.get_event_loop()
+
 def home(request):
+    is_true = False
+
+    # if request.method == 'POST':
+        # loop = asyncio.new_event_loop()
+        # asyncio.set_event_loop(loop)
+        # rate_data = loop.run_until_complete(getRate('https://www.amazon.com/Jackson-Safety-Nemesis-22475-Anti-Fog/product-reviews/B008D81A0A/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'))
+        # loop.close()
+        # print(rate_data)
+
+    if request.method == 'POST':
+        jj = request.POST
+        print(request.POST)
+        # print('type', type(jj))
+        # url = {'urls' : 'hi'}
+        # print('AFTER', QuotesSpider(url))
+
+        #TODO: I need to make sure that the 'urls' is a LIST or something or create an if-else statement to see if its a list or not
+        #MORE: I need the TEXTBOX in the HTML to GET BIGGER as the texts get inputted in
+
+        url = jj['urls']
+        x = url.split(',')
+        x = [i.strip() for i in x]
+        print("DJANGO URL", url, x)
+        #url = ','.join(url)
+        fix = ','.join(x)
+        print('DJANGO POST', jj, fix)
+        is_true = True
+        # if is_true:
+        #     subprocess.call(['scrapy', 'crawl', 'amz', '-a', f'domain={fix}'])
+
+        #this gets called quick, just make sure this gets called after an if statement and itll work out
+        #subprocess.call(['scrapy', 'crawl', 'amz'])
+
     return render(request, 'search/page.html')
+    # Use requests_html to scrape data from the response
+
+    # Replace this with your actual scraping code
+    # await main('https://www.amazon.com/Jackson-Safety-Nemesis-22475-Anti-Fog/product-reviews/B008D81A0A/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews')
+  
+    # return HttpResponse(f'Scraped data: hi')
+
+
+# async def close_session():
+#     await asyncio.sleep(0.25)  # Give asyncio tasks time to complete
+#     await loop.shutdown_asyncgens()
+#     loop.stop()
+
+# # Register the close_session function to be called when the Django application exits
+# loop.run_until_complete(loop.create_task(close_session()))
