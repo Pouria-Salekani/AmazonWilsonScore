145d1440a9d3e3e74cc60863f68e292037218471
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2023-03-27 18:07:57 -0700

    fin

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ey+6t4!*!7(s*k1b(%c0r&^+6v+b$fl71ky#2h*4m3485yy*b5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
