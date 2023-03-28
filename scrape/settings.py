ee1d70953f35c6c15f637470ea03d8926d20f5c7
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2023-03-27 18:15:38 -0700

    disable telnet

--- scrape/settings.py
+++ scrape/settings.py
@@ -44,8 +44,8 @@ ROBOTSTXT_OBEY = True
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+#Disable Telnet Console (enabled by default)
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
